b8eea55a20f84898a9b48fe05c1ae88dc40ef51b
Phillip Havea <p@financeable.com.au>
2026-01-23 15:50:54 +1100

    commit

--- src/App.js
+++ src/App.js
@@ -107,20 +107,20 @@ function AppContent() {
     <>
       <AppBar position="fixed" sx={{ boxShadow: 'none' }}>
         <Toolbar>
-          <Button color="inherit" component={Link} to="/" sx={{ display: 'none' }}>Home</Button>
-          <Button color="inherit" component={Link} to="/about" sx={{ display: 'none' }}>About</Button>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/about">About</Button>
           <Button color="inherit" component={Link} to="/questions">Questions</Button>
           <Button color="inherit" component={Link} to="/services">Services</Button>
           <Button color="inherit" component={Link} to="/pricing">Pricing</Button>
           <Button 
             variant="contained" 
             size="small" 
-            sx={{ ml: 2, backgroundColor: '#002060', '&:hover': { backgroundColor: '#001a47' }, display: 'none' }}
+            sx={{ ml: 2, backgroundColor: '#002060', '&:hover': { backgroundColor: '#001a47' } }}
             onClick={handleNewPriceClick}
           >
             New Price
           </Button>
-          {!activeUser && <Button color="inherit" component={Link} to="/login" sx={{ display: 'none' }}>Login</Button>}
+          {!activeUser && <Button color="inherit" component={Link} to="/login">Login</Button>}
           {activeUser && (
             <>
               <Typography sx={{ flexGrow: 1, ml: 2 }}>{activeUser.email}</Typography>
